88eea18bc8e14b754fc8d9784a46ffde59db4b17
akaganaydin <akagan.aydn@gmail.com>
2024-02-23 11:08:31 +0300

    New project are finished

---
+++ Solution1/Project2/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("I have been a stranger in a stranger land.");
+
+	return 0;
+ }
---
+++ Solution1/Project5/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("\"Hey,\" said the snail, \"I said no salt!\"\n");
+	return 0;
+
+ }
